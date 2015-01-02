a47c4d6181b73408f05ac9fe7d6345f7ac8e42d2
Guanlei <wangguanlei.buaa@gmail.com>
2015-01-01 16:31:40 -0800

    Add comments into updateDB.py

--- updateDB/updateDB.py
+++ updateDB/updateDB.py
@@ -7,21 +7,23 @@ import random
 import names
 from datetime import date, datetime,timedelta
 
-def randomDate(start, end):
+def random_date(start, end):
+    #Generate a random date between start and end
     delta = end - start
     int_delta = (delta.days *24*60*60) + delta.seconds
     random_second = random.randrange(int_delta)
     return start + timedelta(seconds = random_second)
 
-def updateTable(host, user, passwd, database):
+def update_table(host, user, passwd, database):
+    #Get a connection to MySQL db
     con = mdb.connect(host,user,passwd,database)
 
     with con:
         cur = con.cursor()
         cur.execute("SELECT COUNT(*) from tbluser")
         result = str(cur.fetchone())
-        originalRowCount = int(result[1:-3])
-        print "Table tbluser has "+ str(originalRowCount)+ " records."    
+        original_row_count = int(result[1:-3])
+        print "Table tbluser has "+ str(original_row_count)+ " records."    
 
         i = 0
         while i < 1000:
@@ -30,74 +32,76 @@ def updateTable(host, user, passwd, database):
             firstname = names.get_first_name()
             if len(firstname) > 50:
                 firstname = firstname[0:49]
-            firstnameStr = '"' + firstname + '"'
+            firstname_str = '"' + firstname + '"'
 
             lastname = names.get_last_name()
             if len(lastname) > 50:
                 lastname = lastname[0:49]
-            lastnameStr = '"' + lastname + '"'
+            lastname_str = '"' + lastname + '"'
 
             #Generate random unique username(a string containing letters and digits,lenght is 25 chars): varchar(25)
             username = ''.join(random.choice(string.ascii_letters + string.digits)\
                        for _ in range(25))
-            usernameStr = '"' + username + '"'
+            username_str = '"' + username + '"'
     
             #Generate random password(a string containing letters and digits,with a random length of 8 to 12):varchar(128)
             size = random.randint(8,12)
             password = ''.join(random.choice(string.ascii_letters + string.digits)\
                        for _ in range(size))
-            passwordStr = '"' + password + '"'
+            password_str = '"' + password + '"'
 
             #Generate person height: random value between 50 to 250cm maybe: double 
             height = random.uniform(50,250)
     
             #Generate random birthdate: date between 1915-1-1 and 2000-12-31 (according to lifesum signup page)
-            startDate = date(1915,1,1)
-            endDate = date(2000,12,31)
-            birthDate = randomDate(startDate, endDate)
-            birthDateStr = '"' + datetime.strftime(birthDate,'%Y-%m-%d') + '"'
+            start_date = date(1915,1,1)
+            end_date = date(2000,12,31)
+            birth_date = random_date(start_date, end_date)
+            birth_date_str = '"' + datetime.strftime(birth_date,'%Y-%m-%d') + '"'
 
             #Calculate age from birthDate
             today = date.today()
-            age = today.year - birthDate.year - ((today.month, today.day) \
-                  < (birthDate.month, birthDate.day))
+            age = today.year - birth_date.year - ((today.month, today.day) \
+                  < (birth_date.month, birth_date.day))
 
             #Generate random last sync time: datetime between 2008-1-1 0:0:0 to now
-            startTime = datetime.strptime('1/1/2008 0:0:0', '%m/%d/%Y %H:%M:%S')
+            start_time = datetime.strptime('1/1/2008 0:0:0', '%m/%d/%Y %H:%M:%S')
             now = datetime.strftime(datetime.now(),'%m/%d/%Y %H:%M:%S')
-            endTime = datetime.strptime(now, '%m/%d/%Y %H:%M:%S')
-            lastsync = randomDate(startTime, endTime)
-            lastsyncStr = '"'+datetime.strftime(lastsync, '%Y-%m-%d %H:%M:%S')+'"'
+            end_time = datetime.strptime(now, '%m/%d/%Y %H:%M:%S')
+            lastsync = random_date(start_time, end_time)
+            lastsync_str = '"'+datetime.strftime(lastsync, '%Y-%m-%d %H:%M:%S')+'"'
 
             #Generate random device from enum
             device = random.choice(('android','online','apple'))
-            deviceStr = '"' + device + '"'
+            device_str = '"' + device + '"'
 
             #insert records to db
             cur.execute("INSERT INTO tbluser(firstname, lastname, username, height,\
                     age, password, lastsync, device, birthdate) VALUES(%s,%s,\
-                    %s,%.2f,%i,%s,%s,%s,%s)" %(firstnameStr,lastnameStr,\
-                    usernameStr, height,age,passwordStr,lastsyncStr,\
-                    deviceStr,birthDateStr))
+                    %s,%.2f,%i,%s,%s,%s,%s)" %(firstname_str,lastname_str,\
+                    username_str, height,age,password_str,lastsync_str,\
+                    device_str,birth_date_str))
             i += 1
 
         cur.execute("SELECT COUNT(*) from tbluser")
         result = str(cur.fetchone())
-        currentRowCount = int(result[1:-3])
-        print "Table tbluser has "+str(currentRowCount)+ " records now."
+        current_row_count = int(result[1:-3])
+        print "Table tbluser has "+str(current_row_count)+ " records now."
 
-        if currentRowCount - originalRowCount == 1000:
+        if current_row_count - original_row_count == 1000:
             print "Insert 1000 random records successed!"
         else:
             print "Insert 1000 random records failed! Please check!"
 
 def printTimeCost(start, end):
-    timeDelta = end - start
-    timeCost = (timeDelta.days *24*60*60) + timeDelta.seconds
+    #Print the time cost for insert 1000 records
+    time_delta = end - start
+    time_cost = (time_delta.days *24*60*60) + time_delta.seconds
 
-    print ("The operation cost " + str(timeCost) + " seconds")
+    print ("The operation cost " + str(time_cost) + " seconds")
 
 def main(argv):
+    #Below is the command line parameters
     server = ''
     user = ''
     passwd = ''
@@ -123,10 +127,11 @@ def main(argv):
             db = arg
 
     start1 = datetime.now()
-
-    updateTable(server, user, passwd, db)
+    #call update_table to insert 1000 recodes
+    update_table(server, user, passwd, db)
     
     end1 = datetime.now()
+    #print the time cost
     printTimeCost(start1, end1)
 
 if __name__ == "__main__":
